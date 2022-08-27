f15739309451bac50ed7632d29f498ca91ae2d9e
Ubuntu <ubuntu@ip-172-31-92-211.ec2.internal>
2022-08-27 13:55:26 +0000

    Removed images and Dynamo, need to fix hard coded IPs and navigation

--- EmpApp.py
+++ EmpApp.py
@@ -2,27 +2,23 @@ from flask import Flask, render_template, request
 from pymysql import connections
 import os
 import boto3
-from config import *
+
 
 app = Flask(__name__)
 
-# DBHOST = os.environ.get("DBHOST")
-# DBPORT = os.environ.get("DBPORT")
-# DBPORT = int(DBPORT)
-# DBUSER = os.environ.get("DBUSER")
-# DBPWD = os.environ.get("DBPWD")
-# DATABASE = os.environ.get("DATABASE")
+DBHOST = os.environ.get("DBHOST")
+DBPORT = int(os.environ.get("DBPORT"))
+DBUSER = os.environ.get("DBUSER")
+DBPWD = os.environ.get("DBPWD")
+DATABASE = os.environ.get("DATABASE")
 
-bucket= custombucket
-region= customregion
-table= customtable
 
 db_conn = connections.Connection(
-    host= customhost,
-    port=3306,
-    user= customuser,
-    password= custompass,
-    db= customdb
+    host= DBHOST,
+    port=DBPORT,
+    user= DBUSER,
+    password= DBPWD, 
+    db= DATABASE
     
 )
 output = {}
@@ -42,65 +38,16 @@ def AddEmp():
     last_name = request.form['last_name']
     pri_skill = request.form['pri_skill']
     location = request.form['location']
-    emp_image_file = request.files['emp_image_file']
+
   
     insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
     cursor = db_conn.cursor()
-
-    if emp_image_file.filename == "":
-        return "Please select a file"
 
     try:
         
         cursor.execute(insert_sql,(emp_id, first_name, last_name, pri_skill, location))
         db_conn.commit()
         emp_name = "" + first_name + " " + last_name
-        # Uplaod image file in S3 #
-        emp_image_file_name_in_s3 = "emp-id-"+str(emp_id) + "_image_file"
-        s3 = boto3.resource('s3')
-
-        
-        
-        try:
-            print("Data inserted in MySQL RDS... uploading image to S3...")
-            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
-            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-            s3_location = (bucket_location['LocationConstraint'])
-
-            if s3_location is None:
-                s3_location = ''
-            else:
-                s3_location = '-' + s3_location
-
-            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
-                s3_location,
-                custombucket,
-                emp_image_file_name_in_s3)
-
-            # Save image file metadata in DynamoDB #
-            print("Uploading to S3 success... saving metadata in dynamodb...")
-        
-            
-            try:
-                dynamodb_client = boto3.client('dynamodb', region_name= customregion )
-                dynamodb_client.put_item(
-                 TableName= customtable,
-                    Item={
-                     'empid': {
-                          'N': emp_id
-                      },
-                      'image_url': {
-                            'S': object_url
-                        }
-                    }
-                )
-
-            except Exception as e:
-                program_msg = "Flask could not update DynamoDB table with S3 object URL"
-                return str(e)
-        
-        except Exception as e:
-            return str(e)
 
     finally:
         cursor.close()
@@ -124,7 +71,8 @@ def FetchData():
     try:
         cursor.execute(select_sql,(emp_id))
         result = cursor.fetchone()
-
+        
+        # Add No Employee found form
         output["emp_id"] = result[0]
         print('EVERYTHING IS FINE TILL HERE')
         output["first_name"] = result[1]
@@ -132,22 +80,7 @@ def FetchData():
         output["primary_skills"] = result[3]
         output["location"] = result[4]
         print(output["emp_id"])
-        dynamodb_client = boto3.client('dynamodb', region_name=customregion)
-        try:
-            response = dynamodb_client.get_item(
-                TableName= customtable ,
-                Key={
-                    'empid': {
-                        'N': str(emp_id)
-                    }
-                }
-            )
-            image_url = response['Item']['image_url']['S']
-
-        except Exception as e:
-            program_msg = "Flask could not update DynamoDB table with S3 object URL"
-            return render_template('addemperror.html', errmsg1=program_msg, errmsg2=e)
-
+        
     except Exception as e:
         print(e)
 
@@ -155,8 +88,7 @@ def FetchData():
         cursor.close()
 
     return render_template("GetEmpOutput.html", id=output["emp_id"], fname=output["first_name"],
-                           lname=output["last_name"], interest=output["primary_skills"], location=output["location"],
-                           image_url=image_url)
+                           lname=output["last_name"], interest=output["primary_skills"], location=output["location"])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=80,debug=True)
+    app.run(host='0.0.0.0',port=8080,debug=True)
