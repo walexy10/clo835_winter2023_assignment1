2f89f94ccb796ec01f72b42df8d499f0985ab7a2
Ubuntu <ubuntu@ip-172-31-45-187.ec2.internal>
2022-09-10 18:29:36 +0000

    Added the code

--- mysql.sql
+++ mysql.sql
@@ -1,14 +1,16 @@
-CREATE USER 'admin'@'%' IDENTIFIED  BY 'passwors';
-GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+DROP DATABASE IF EXISTS employees;
 CREATE DATABASE employees;
 USE employees;
+
+DROP TABLE IF EXISTS employee;
 CREATE TABLE employee(
 emp_id VARCHAR(20),
 first_name VARCHAR(20),
 last_name VARCHAR(20),
-pri_skill VARCHAR(20),
+primary_skill VARCHAR(20),
 location VARCHAR(20));
+
 INSERT INTO employee VALUES ('1','Amanda','Williams','Smile','local');
 INSERT INTO employee VALUES ('1','Alan','Williams','Empathy','alien');
+SELECT * FROM employee;
 
