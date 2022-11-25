2f477a4c5646f2dfe5738cc4ae09e6314deacbc0
igeiman13 <101915037+igeiman13@users.noreply.github.com>
2022-11-24 19:48:06 -0500

    Updated initialization script to keep the schema

--- mysql.sql
+++ mysql.sql
@@ -1,9 +1,7 @@
-DROP DATABASE IF EXISTS employees;
-CREATE DATABASE employees;
+CREATE DATABASE IF NOT EXISTS employees;
 USE employees;
 
-DROP TABLE IF EXISTS employee;
-CREATE TABLE employee(
+CREATE TABLE IF NOT EXISTS employee(
 emp_id VARCHAR(20),
 first_name VARCHAR(20),
 last_name VARCHAR(20),
