7061a375f30d44b8c81185c6db0288b58a709c3c
EC2 Default User <ec2-user@ip-172-31-60-165.ec2.internal>
2023-02-08 18:59:21 +0000

    new commits

---
+++ terraform_code/modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "walexy"
+    "App"   = "Web"
+    "Project" = "CLO835"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "Ass1"
+}
